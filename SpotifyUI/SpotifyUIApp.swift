94f94fac6322c274bf6988dd194aaa02c49da28f
Avinash Kumar <140255961+araj54657@users.noreply.github.com>
2026-01-26 17:57:50 +0530

    Initial Commit

---
+++ SpotifyUI/SpotifyUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyUIApp.swift
+//  SpotifyUI
+//
+//  Created by Avinash Kumar on 26/01/26.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpotifyUITests/SpotifyUITests.swift
@@ -0,0 +1,16 @@
+//
+//  SpotifyUITests.swift
+//  SpotifyUITests
+//
+//  Created by Avinash Kumar on 26/01/26.
+//
+
+import Testing
+
+struct SpotifyUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
